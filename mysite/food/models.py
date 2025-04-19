9272993fd9fb9d87ad450251b20cd2352f50f0af
Lokesh Kumar ARUDRA <103440456+LokeshKumar-Arudra@users.noreply.github.com>
2025-04-19 19:34:04 +0000

    Adding Images fied and rendering them in index.html

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -9,3 +9,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500 , default="https://cdn.pixabay.com/photo/2021/04/06/15/00/pie-6156640_1280.jpg")
+
